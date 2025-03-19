3541c05e29e5a63a6daa8d7b7e86cb21d4fc4900
Dorian Garde <dorian.garde@tbdgroup.com>
2025-03-19 17:05:00 +0100

    Upgrade api version from 26 to 34

--- class/PaylinePaymentGateway.php
+++ class/PaylinePaymentGateway.php
@@ -13,7 +13,7 @@ class PaylinePaymentGateway
     private static $merchantSettings = null;
 
     // API version to pass on WS call
-    const API_VERSION = 26;
+    const API_VERSION = 34;
 
     const WEB_PAYMENT_METHOD = 1;
     const RECURRING_PAYMENT_METHOD = 2;
